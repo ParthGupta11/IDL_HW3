aabd0952af65e7120c3883e2b60a2569f694cefc
parth <parthg@andrew.cmu.edu>
2025-10-31 20:53:26 -0400

    Attempt MCQ

---
+++ standard/MCQ/mcq.py
@@ -0,0 +1,18 @@
+def question_1():
+    return "B"
+
+
+def question_2():
+    return "D"
+
+
+def question_3():
+    return "B"
+
+
+def question_4():
+    return "C"
+
+
+def question_5():
+    return "A"
